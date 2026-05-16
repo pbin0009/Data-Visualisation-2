09dc9feae00aedb3ab53b734502fb84b7b96e9a3
pbin0009 <pbin0009@student.monash.edu>
2026-05-16 22:15:46 +1000

    finish fast food section

--- js/fastFoodMap_vg.js
+++
@@ -1,5 +0,0 @@
-var fast_food_map = "jsonFile/fast_food_map.vg.json";
-
-vegaEmbed("#dot-map", fast_food_map).then(function(result) {
-    // Access Vega view using result.view
-}).catch(console.error);
